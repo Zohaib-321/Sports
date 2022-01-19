1b041d720cc4bd12c7f56679f5be0bacd50002ca
Zohaib Mughal <zohaibmughalcs@gmail.com>
2022-01-19 18:31:50 +0500

    added seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Sports.Repo
+
+alias Sports.Arenas.Arena
+
+
+
+
+
+
+# maintanance
+Repo.insert!(%Arena{
+  name: "zohaib",
+  opens_at: 42,
+  closes_at: 42,
+  price_per_hour: 42,
+  address: %{street: "exam", city: "laoe", country: "koo", state: "djk"},
+  phone_number: %{country_code: 92, area_code: 00, number: 03204466051},
+
+})
